e7b8a3796e126f76f387841f22c5fb57ceb430b3
TayLee <thlee991@gmail.com>
2023-05-02 13:12:25 +0900

    commit)

---
+++ mngtapi/src/main/java/com/manage/apigw/mngtapi/MngtapiApplication.java
@@ -0,0 +1,13 @@
+package com.manage.apigw.mngtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngtapiApplication.class, args);
+	}
+
+}
