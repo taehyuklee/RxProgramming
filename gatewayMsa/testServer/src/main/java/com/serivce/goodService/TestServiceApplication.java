7e510f63a4d980b3ff009512bc7ce3d19be64ed0
TayLee <thlee991@gmail.com>
2023-08-02 09:54:55 +0900

    commit) test server for GS

---
+++ gatewayMsa/testServer/src/main/java/com/serivce/goodService/TestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.serivce.goodService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServiceApplication.class, args);
+	}//3.34.225.6:
+
+}
---
+++ gatewayMsa/testServer/src/main/java/com/serivce/goodService/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.serivce.goodService.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+//Controller
+@RestController //화면을 갖고있지 않고, RestAPI 형태로만 신호가 왔다 갔다 할거임. 
+@RequestMapping("/first-service") //사용자로부터 요청되는 URI값을 지정해두는 것.
+@Slf4j // log로 바로 출력해서 볼수 있도록 한다.
+public class TestController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to the First service";
+    }
+
+    @GetMapping("/error") //first-request header를 받는다.
+    public String error(@RequestParam(required = false) String message){
+        System.out.println(message);
+        log.info("Fail");
+        return "You're request is success.";
+    }
+
+    @GetMapping("/ok") //first-request header를 받는다.
+    public String ok(@RequestParam(required = false) String message){
+        System.out.println(message);
+        log.info("Success");
+        return "your request is fail.";
+    }
+
+}
+//Controller와 RestContorller의 차이는 RequestBody와 ResponseBody를 구현하느냐 아니냐의 차이점이다.
