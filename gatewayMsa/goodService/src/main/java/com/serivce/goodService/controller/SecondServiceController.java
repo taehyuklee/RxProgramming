7cdc48aea95d05490064356811ff9e0e622c5995
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-10-16 01:09:15 +0900

    commit) 커밋

--- gatewayMsa/goodService/src/main/java/com/serivce/goodService/controller/SecondServiceController.java
+++ gatewayMsa/goodService/src/main/java/com/serivce/goodService/controller/SecondServiceController.java
@@ -1,13 +1,18 @@
 package com.serivce.goodService.controller;
 
-import java.util.Enumeration;
+import java.util.*;
+import java.util.Map.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.util.StringBuilders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,18 +21,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j // log로 바로 출력해서 볼수 있도록 한다.
 public class SecondServiceController {
     @GetMapping("/welcome")
-    public String welcome(HttpServletRequest request, HttpServletResponse response){
+    public String welcome(HttpServletRequest request, HttpServletResponse response) throws JsonProcessingException{
+
+        log.info("null");
+        Map<String, String> responseHeader = new HashMap<>();
 
         Enumeration<String> headerNm = request.getHeaderNames();
         while (headerNm.hasMoreElements()) {
             String headerName = headerNm.nextElement();
             String headerValue = request.getHeader(headerName);
-            
+            responseHeader.put(headerName, headerValue);
+
             // 각 헤더를 응답에 추가
             response.setHeader(headerName, headerValue);
         }
 
-        return "Jinsol's Test 중입니다";
+        // JSON 문자열로 변환
+
+
+        return  makeJson(responseHeader);
+    }
+
+    String makeJson(Map<String, String> responseHeader ){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[request에서 response에 추가된 header들 목록들] \n\n");
+        for(Entry<String, String> entry : responseHeader.entrySet()){
+            sb.append(entry.getKey()).append(" : ").append(entry.getValue()).append(",").append("\n");
+        }
+       
+        return sb.toString();
     }
 
 }
